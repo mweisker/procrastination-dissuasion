cf9e0dd3c99af40617e688d6c380ed30f83f97e3
Matthew Weisker <mweisker27@gmail.com>
2023-06-21 11:52:08 -0700

    Ready to get started

--- server/server.js
+++ server/server.js
@@ -1,31 +1,18 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
-PORT = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
 
-// create static
-// app.use(express.static(path.resolve(__dirname, '../public')))
+app.use('/', express.static(path.join(__dirname, '../assets')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../../public/index.html'))
-// })
-
+// statically serve everything in the build folder on the route '/build'
 app.use('/build', express.static(path.join(__dirname, '../build')));
-
+// serve index.html on the route '/'
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
 
 
-// 404 handler
-app.use('*', (req, res) => {
-  res.status(404).send('Not Found');
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}...`);
-});
+app.listen(3000); //listens on port 3000 -> http://localhost:3000/
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,47 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: {
-    src: './client/index.js',
-  },
+  mode:  process.env.NODE_ENV, //sets the mode for webpack to determine how it optimizes the bundles
+  entry: './client/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
-  plugins: [
-    new HTMLWebpackPlugin({
-      title: 'Development',
-      template: 'index.html',
-    }),
-  ],
   module: {
     rules: [
       {
-        test: /\.jsx?/,
+        test: /\.js/, 
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/env', ['@babel/react', {"runtime": "automatic"}]]
-        },
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
       },
       {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: ["style-loader", "css-loader"]
-    },
-    ],
-  },
-  devServer: {
-    static: {
-      publicPath: '/',
-      directory: path.resolve(__dirname, 'build')
-    },
-    hot: true,
+        test: /\.s[ac]ss/, //.sass or .scss 
+        use: [ 
+          "style-loader", 'css-loader', 'sass-loader'
+        ] 
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: "asset",
+        use: [ 'file-loader'
+        ],
+      }
+    ]
+  }, 
+  plugins: [ new HtmlWebpackPlugin({
+    title: 'Development',
+    template: 'index.html'
+   })
+], 
+  devServer: { 
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/': 'http://localhost:3000'
     }
-  },
-  watch: true,
+  }
 };
